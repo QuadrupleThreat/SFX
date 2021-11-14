75bac0303817448b0418f46a485f29c82b171be4
7daylenrobertson <9daylenrobertson@gmail.com>
2021-11-14 03:35:58 -0500

    updated grab function

--- database_functions.py
+++ database_functions.py
@@ -1,4 +1,7 @@
 import pyrebase #pip dependacies: pyrebase
+import keyboard
+import time
+
 
 #auth configuration
 firebaseConfig={"apiKey": "AIzaSyDr4vVZdvqv8-Kklyy-wfTE1rpNclYpvAs",
@@ -25,10 +28,25 @@ def sendtext(username, message):
     data={"text": message}
     db.child(username).set(data) #sends text, other user needs username to pull texts from user
 
-username="coolsignguy"
+username="hearing_imp"
 message="How are you"
-
+oldmessage=""
 #sendtext(username,message)
-def grabtext(username,message):
-    user=db.child(username).get().val()
-    print(list(user.values())[0]) #user is ordered dic, gives me text value
+def grabtext(username,oldmessage):
+    while True:
+      if keyboard.is_pressed('q'):
+        break
+      else:
+        time.sleep(4)   
+        if oldmessage=="":
+          print(list(db.child(username).get().val().values())[0]) 
+          oldmessage=list(db.child(username).get().val().values())[0] #switches message out to allow a new message to be sent
+
+        elif list(db.child(username).get().val().values())[0]!=oldmessage:
+          print(list(db.child(username).get().val().values())[0])
+          oldmessage=list(db.child(username).get().val().values())[0]
+          
+
+    #print(list(user.values())[0]) #user is ordered dic, gives me text value
+
+grabtext(username,oldmessage)
